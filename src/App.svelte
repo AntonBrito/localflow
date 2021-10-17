63e61a3a3c4613c49aaa7291394d2834a3e8c14f
AntonBrito <tonybrito85@gmail.com>
2021-10-17 13:21:02 -0400

    Added data , and organized workiflow

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,31 @@
 <script>
-	export let name;
+	import ReviewList from "./components/ReviewList.svelte"
+
+
+	let review = [
+		{
+			id: 1,
+			rating: 10,
+			text: 'Lorem ipsum dolor sir amet consecteur adipisicing elit.consecteur vel avitae commodi alias voluptartem est voluptatum ipsa quae.',
+		},
+		{
+			id: 2,
+			rating: 7,
+			text: 'Lorem ipsum dolor sir amet consecteur adipisicing elit.consecteur vel avitae commodi alias voluptartem est voluptatum ipsa quae.',
+		},{
+			id: 3,
+			rating: 5,
+			text: 'Lorem ipsum dolor sir amet consecteur adipisicing elit.consecteur vel avitae commodi alias voluptartem est voluptatum ipsa quae.',
+		}
+	]
+
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	
+	<ReviewList {review}/>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+	
 </style>
